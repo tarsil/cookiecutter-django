4a0905bd7c067772695926eedb99a874f100a8b5
Tiago A. Silva <tiagoarasilva@users.noreply.github.com>
2018-01-31 16:20:46 +0000

    Update tasks

--- roles/tasks.py
+++ roles/tasks.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 
@@ -5,8 +6,21 @@ from jinja2 import Environment, FileSystemLoader
 from invoke import run, task
 
 
+def _template_file(template, destination, template_dir='/var/www/deploy/'):
+    environment = Environment(loader=FileSystemLoader(template_dir))
+    template = environment.get_template(template)
+    rendered_template = template.render(os.environ)
+    if os.path.isfile(destination):
+        os.unlink(destination)
+    with open(destination, 'w') as f:
+        f.write(rendered_template)
+
+
 @task
-def development(context):
+def development():
     run('pip3 install -U pip')
     run('pip3 install -r requirements/development.txt')
+    # _template_file('nginx/nginx.conf', '/etc/nginx/sites-enabled/default')
+    # _template_file('supervisor.nginx.conf', '/etc/supervisor/conf.d/nginx.conf')
+    # _template_file('supervisor.celery.conf', '/etc/supervisor/conf.d/celery.conf')
     run('supervisord -n')
