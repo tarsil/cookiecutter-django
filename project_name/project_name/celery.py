ef687c1b899a786617216e88cc83f18132ca756b
Tiago A. Silva <tiagoarasilva@users.noreply.github.com>
2018-03-16 13:36:19 +0000

    Settings changed for django module

--- project_name/project_name/celery.py
+++ project_name/project_name/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saturn.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 
 CELERY_TIMEZONE = 'UTC'
