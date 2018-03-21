4fcd84ae7ed369026c80cd646089308f9f545146
Tiago Silva <tiago.silva@incoproip.com>
2018-03-21 12:47:39 +0000

    URLs fixed to reflect removed views

--- project_name/accounts/urls.py
+++ project_name/accounts/urls.py
@@ -8,10 +8,5 @@ app_name = AccountsConfig.name
 
 
 accounts_urlpatterns = [
-    re_path(r'^add-user/$', accounts.views.RegisterProfileView.as_view(), name='add-user'),
-    re_path(r'^view/(?P<slug>[a-zA-Z0-9-_]+)/$', accounts.views.ProfileView.as_view(), name="view-profile"),
-    re_path(r'^edit/(?P<slug>[a-zA-Z0-9-_]+)/$', accounts.views.ProfileEditView.as_view(), name="edit-profile"),
-    re_path(r'^(?P<slug>[a-zA-Z0-9-_]+)/change-password/$', accounts.views.ChangePasswordView.as_view(), name="change-password"),
-    re_path(r'^list-users/$', accounts.views.ListUsersView.as_view(), name='list-users'),
-    re_path(r'^settings/(?P<slug>[a-zA-Z0-9-_]+)/$', accounts.views.SettingsView.as_view(), name='settings'),
+    re_path(r'^user/create/$', accounts.views.RegisterProfileView.as_view(), name='add-user'),
 ]
