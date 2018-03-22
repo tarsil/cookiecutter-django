db5ecbddb944ffec55bee082dc605a87b24c65f2
Tiago Silva <tiago.silva@incoproip.com>
2018-03-22 10:36:12 +0000

    Docker-compose updated, loggind added

--- project_name/project_name/general/settings.py
+++ project_name/project_name/general/settings.py
@@ -27,3 +27,65 @@ LANGUAGES = (
 # Pseudolanguage is used for Enabling Crowdin translation mode
 PSEUDO_LANGUAGE = 'zu'
 VISIBLE_LANGUAGES = tuple(x for x in LANGUAGES if x[0] != PSEUDO_LANGUAGE)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    'formatters': {
+        'json': {
+            '()': 'pythonjsonlogger.jsonlogger.JsonFormatter',
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    "handlers": {
+        'bugsnag': {
+            'level': 'ERROR',
+            'class': 'bugsnag.handlers.BugsnagHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'json',
+        },
+        'investigation': {
+            "level": "INFO",
+            "class": "bugsnag.handlers.BugsnagHandler"
+        }
+    },
+    "loggers": {
+        "django.request": {
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "celery": {
+            "handlers": ["console", "bugsnag"],
+            "level": "INFO",
+            "propagate": True
+        },
+        "httpstream": {
+            "handlers": ["console", "bugsnag"],
+            "level": "ERROR",
+            "propagate": True
+        },
+        "elasticsearch": {
+            "handlers": ["console", "bugsnag"],
+            "level": "WARNING",
+            "propagate": False
+        },
+        "urllib3": {
+            "handlers": ["console", "bugsnag"],
+            "level": "WARNING",
+            "propagate": False
+        },
+        "celery.investigation": {
+            "level": "INFO",
+            "handlers": ["investigation"],
+            "propagate": False
+        }
+
+    }
+}
+
