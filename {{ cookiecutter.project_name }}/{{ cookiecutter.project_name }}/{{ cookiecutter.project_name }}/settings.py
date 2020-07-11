7ab74b8ec7393b7deba8faca200c5da95c7c76ba
Tiago A. Silva <tiagoarasilva@users.noreply.github.com>
2020-07-11 20:09:31 +0100

    Update settings.py

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/settings.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/settings.py
@@ -73,16 +73,18 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-
-    ],
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 20
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'PAGE_SIZE': 50
 }
 
 
