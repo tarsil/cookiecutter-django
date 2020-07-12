3875c82a02efc5ecb858289ded40ddf843f977f5
Tiago A. Silva <tiagoarasilva@users.noreply.github.com>
2020-07-12 15:35:31 +0100

    Update settings.py

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/settings.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/settings.py
@@ -76,9 +76,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
